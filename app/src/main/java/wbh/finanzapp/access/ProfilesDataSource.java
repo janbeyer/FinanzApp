83c127a2f54f09238cdd1b4852ceda67264cfa98
Beyer, Jan <Jan.Beyer@dsa.de>
2018-10-17 13:07:23 +0200

    Add isDbOpen
    Add the isDbOpen() function to ProfilesDataSource

--- app/src/main/java/wbh/finanzapp/access/ProfilesDataSource.java
+++ app/src/main/java/wbh/finanzapp/access/ProfilesDataSource.java
@@ -22,7 +22,15 @@ public class ProfilesDataSource {
     /**
      * Indicate if this data bas is open.
      */
-    boolean isDbOpen = false;
+    private boolean isDbOpen = false;
+
+    /**
+     * Check if this DB is already open.
+     * @return true if the DB is open, else false.
+     */
+    public boolean isDbOpen() {
+        return isDbOpen;
+    }
 
     private String[] columns = {
         ProfilesDBHelper.COLUMN_ID,
