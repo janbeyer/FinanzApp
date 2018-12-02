57640b399e3cf2bec9cb8fdfe40eccd4402eaf63
swenkrause <43118640+swenkrause@users.noreply.github.com>
2018-12-02 14:31:25 +0100

    #36 UnitTest
    Set up UnitTest environment for the different modules
    
    DBHelper
    AnalysisBean
    GroupBean
    TransactionBean

---
+++ app/src/test/java/wbh/finanzapp/business/AnalysisBeanTest.java
@@ -0,0 +1,34 @@
+package wbh.finanzapp.business;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AnalysisBeanTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getTotal() {
+    }
+
+    @Test
+    public void setTotal() {
+    }
+
+    @Test
+    public void getGroups() {
+    }
+
+    @Test
+    public void setGroups() {
+    }
+}
